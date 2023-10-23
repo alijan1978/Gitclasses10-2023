fb0ef76f2578cb8b9f2b432d221eb99b957fbf55
alijan1978 <mohammadalinaim32@gmail.com>
2023-10-23 11:36:15 -0700

    I want to push the Monday class material

--- src/GITclass1/GitPractice1.java
+++ src/GITclass1/GitPractice1.java
@@ -4,7 +4,10 @@ public class GitPractice1 {
     public static void main(String[] args) {
 
         System.out.println("Welcome to GiT practice");
+
         System.out.println("I learned pushing code to Git, and was able to commit my Java class into my local repository");
+        System.out.println("Today is Monday the second GiT class material I learned");
+
     }
 }
 
