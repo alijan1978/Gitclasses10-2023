e5cebf973bdb8c99fe18e40af255dec7974e0560
Ali <mohammadalinaim32@gmail.com>
2023-10-23 11:08:20 -0700

    All the Git classes are pushed to repo

--- src/GITclass1/GitPractice1.java
+++ src/GITclass1/GitPractice1.java
@@ -4,5 +4,8 @@ public class GitPractice1 {
     public static void main(String[] args) {
 
         System.out.println("Welcome to GiT practice");
+        System.out.println("I learned pushing code to Git, and was able to commit my Java class into my local repository");
     }
 }
+
+// video 2 min 1:08
